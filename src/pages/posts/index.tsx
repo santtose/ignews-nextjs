0902f2e435171652beeb2a7031487a821a006189
santtose <santtose@outlook.com>
2021-10-26 10:09:46 -0300

    listing posts on screen

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,22 @@
 import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,29 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Aqui vai um titulo</strong>
-            <p>Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo </p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Aqui vai um titulo</strong>
-            <p>Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo </p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Aqui vai um titulo</strong>
-            <p>Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo </p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Aqui vai um titulo</strong>
-            <p>Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo Aqui vai um paragrafo </p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }
         </div>
       </main>
     </>
@@ -52,9 +48,22 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   )
 
-  console.log(response);
+  const posts = response.results.map( post => {
+    return {
+     slug: post.uid,
+     title: RichText.asText(post.data.title),
+     excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+     updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+       day: '2-digit',
+       month: 'long',
+       year: 'numeric'
+     }) 
+    };
+  });
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
